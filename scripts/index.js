201689ecc96bdc90af81e0cf1a5e681f86e00fde
unknown <Jeisonlopezarroyave@gmail.com>
2024-02-01 02:44:06 -0500

    Dom

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,71 @@ class Repository {
     this.activities = this.activities.filter((activity) => activity.id !== id);
   }
 }
+
+const repository = new Repository();
+
+function createHTMLFromActivity(activity) {
+  const { title, description, imgurl } = activity;
+
+  const activityDiv = document.createElement("div");
+
+  const titleElement = document.createElement("h3");
+  titleElement.textContent = title;
+
+  const descriptionElement = document.createElement("p");
+  descriptionElement.textContent = description;
+
+  const imageElement = document.createElement("img");
+  imageElement.src = imgurl;
+  imageElement.alt = title;
+  imageElement.width = 100;
+
+  activityDiv.appendChild(titleElement);
+  activityDiv.appendChild(descriptionElement);
+  activityDiv.appendChild(imageElement);
+
+  return activityDiv;
+}
+
+function renderActivities(repository, containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
+  const activities = repository.getAllActivities();
+  const activityElements = activities.map(createHTMLFromActivity);
+  activityElements.forEach((activityElement) => {
+    container.appendChild(activityElement);
+  });
+}
+
+function handleAddActivityClick(event) {
+  event.preventDefault();
+  const titleInput = document.getElementById("titleInput");
+  const descriptionInput = document.getElementById("descriptionInput");
+  const imgurlInput = document.getElementById("imgurlInput");
+
+  const title = titleInput.value;
+  const description = descriptionInput.value;
+  const imgrul = imgurlInput.value;
+
+  if (!title || !description || !imgurl) {
+    alert("Por favor, completa todos lso campos.");
+    return;
+  }
+
+  const newActivity = new Activity(Date.now(), title, description, imgrul);
+
+  repository.addActivity(newActivity);
+
+  renderActivities(repository, "activityContainer");
+
+  titleInput.value = "";
+  descriptionInput.value = "";
+  imgurlInput.value = "";
+
+  const addButton = document.getElementById("addActivityButton");
+  if (addButton) {
+    addButton.addEventListener("click", handleAddActivityClick);
+  } else {
+    console.error('El botón con ID "addActivityButton" no fue encontrado');
+  }
+}
